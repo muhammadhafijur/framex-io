0d1b05d9a0b564935b54e771393e2e53a4bf335b
MuhammadHafijur <hafijur.salah@gmail.com>
2021-12-24 21:20:12 +0600

    getting started with framex-io project

---
+++ src/Pages/Home/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../../Shared/NavBar/NavBar';
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+        </div>
+    );
+};
+
+export default Home;

ceda8af72953d31bfbbe191bfe8192f70f607541
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-12-25 18:29:25 +0600

    fake data and custom hook added

--- src/Pages/Home/Home/Home.tsx
+++ src/Pages/Home/Home/Home.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import NavBar from '../../Shared/NavBar/NavBar';
+import React from "react";
+import useServices from "../../../hooks/useServices";
+import NavBar from "../../Shared/NavBar/NavBar";
 
 const Home = () => {
-    return (
-        <div>
-            <NavBar></NavBar>
-        </div>
-    );
+  const [services, setServices] = useServices();
+  console.log(services);
+
+  return (
+    <div>
+      <NavBar></NavBar>
+    </div>
+  );
 };
 
 export default Home;

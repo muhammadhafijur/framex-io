0f886230e0291058a588d1e792342528ffeec1a3
MuhammadHafijur <hafijur.salah@gmail.com>
2021-12-25 10:02:39 +0600

    added home nav

--- src/Pages/Shared/NavBar/NavBar.tsx
+++ src/Pages/Shared/NavBar/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar = () => {
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-            <a className="mr-5 hover:text-gray-900">First Link</a>
+            <a className="mr-5 hover:text-gray-900">Home</a>
             <a className="mr-5 hover:text-gray-900">Second Link</a>
             <a className="mr-5 hover:text-gray-900">Third Link</a>
             <a className="hover:text-gray-900">Fourth Link</a>

298242a7d154cd536dd1a440e491da42389535fc
Mohammed Sadik <msadikjowel@gmail.com>
2021-12-25 18:59:05 +0600

    dashboard route updated

--- src/Pages/Dashboard/DashboardHome/DashboardHome.tsx
+++ src/Pages/Dashboard/DashboardHome/DashboardHome.tsx
@@ -52,7 +52,7 @@ const DashboardHome = () => {
                                     </svg>
                                 </span>
                                 <span className="mx-4 text-sm font-normal">
-                                    My Orders
+                                    My Order
                                 </span>
                             </Link>
                             {/* <Link to='/calendar' className="w-full font-thin uppercase text-gray-500 dark:text-gray-200 flex items-center p-4 my-2 transition-colors duration-200 justify-start hover:text-blue-500">

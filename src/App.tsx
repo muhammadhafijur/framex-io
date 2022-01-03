34965f6f4a9a4dcee339299849668cd0cb42b92a
Siam-Parvez <tanmoyparvez23@gmail.com>
2022-01-04 00:23:39 +0600

    details page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import Home from './Pages/Home/Home/Home';
-import Carousel from './Pages/Home/Carousel/Carousel';
-import NotFound from './Pages/Not-Found/NotFound';
-import PrivateRoute from './Pages/PrivateRoute/PrivateRoute';
-import Dashboard from './Pages/Dashboard/Dashboard/Dashboard';
-import Login from './Pages/Login/Login/Login';
-import Register from './Pages/Login/Register/Register';
-import AddServices from './Pages/Dashboard/AddServices/AddServices';
-import Purchase from './Pages/Purchase/Purchase';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home/Home";
+import Carousel from "./Pages/Home/Carousel/Carousel";
+import NotFound from "./Pages/Not-Found/NotFound";
+import PrivateRoute from "./Pages/PrivateRoute/PrivateRoute";
+import Dashboard from "./Pages/Dashboard/Dashboard/Dashboard";
+import Login from "./Pages/Login/Login/Login";
+import Register from "./Pages/Login/Register/Register";
+import AddServices from "./Pages/Dashboard/AddServices/AddServices";
+import Purchase from "./Pages/Purchase/Purchase";
 
 function App() {
   return (
     <div className="">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/home' element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
 
-          <Route path='/shop' element={<Home />} />
-          <Route path='/about-us' element={<Home />} />
-          <Route path='/contact-us' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/purchase' element={<Purchase />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='*' element={<NotFound />} />
-
+          <Route path="/shop" element={<Home />} />
+          <Route path="/about-us" element={<Home />} />
+          <Route path="/contact-us" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/purchase/:id" element={<Purchase />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Pages/Purchase/Purchase.tsx
+++ src/Pages/Purchase/Purchase.tsx
@@ -1,81 +1,122 @@
-import React from 'react';
-import Footer from '../Shared/Footer/Footer';
-import NavBar from '../Shared/NavBar/NavBar';
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useParams } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
+import NavBar from "../Shared/NavBar/NavBar";
+import Rating from "react-rating";
 
 const Purchase = () => {
+  const { id } = useParams();
 
-    // react hook form
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = (data: any) => console.log(data);
+  const [service, setService] = useState<any>({});
 
-    console.log(watch("example"));
+  useEffect(() => {
+    fetch(`https://framex-server.herokuapp.com/api/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, []);
 
-    return (
-        <>
-            <NavBar />
-            <div className='container mx-auto h-screen mt-10'>
-                <h2 className='flex items-center justify-center text-5xl font-bold'>Confirm your booking for Weeding Photography</h2>
-                <div className='flex justify-between flex-wrap mt-20'>
-                    <div className='flex flex-col items-center justify-center max-w-[50%]  p-3'>
+  console.log(service);
 
-                        <img className='w-6/12 rounded-lg' src="https://image.freepik.com/free-photo/groom-putting-ring-bride-s-finger_1157-338.jpg" alt="" />
+  // react hook form
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
 
-                        <h4 className='text-2xl my-5'>Price: $566</h4>
-                        <h4 className='text-justify	'>Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ab repellat quam minus nemo voluptates quos ad magnam dolores aspernatur consectetur quae nisi quibusdam voluptate velit iure impedit, doloremque magni delectus, perspiciatis excepturi nobis. Consequuntur possimus alias ut maxime magnam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ipsam!</h4>
+  console.log(watch("example"));
 
-                    </div>
-
-                    {/* react hook form starts*/}
-
-                    <div className='flex flex-col items-center justify-center min-w-[50%]'>
-                        <h2 className='text-3xl font-bold mb-5'>Billing Information</h2>
-
-                        <form onSubmit={handleSubmit(onSubmit)} className='w-full flex flex-col items-center justify-center'>
-
-                            <input
-                                defaultValue="akbarkha@gmail.com"
-                                {...register("email", { required: true })}
-                                className='border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 w-3/5'
-                                type='email'
-                                placeholder='Your Email'
-                            /> <br />
-
-                            <input
-                                defaultValue="Akbar Kha"
-                                {...register("name", { required: true })}
-                                className='border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 w-3/5'
-                                type='text'
-                                placeholder='Your Name'
-                            /> <br />
-
-                            <input
-                                placeholder='Your Phone'
-                                {...register("phone", { required: true })}
-                                className='border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 w-3/5'
-                                type='number'
-                            /> <br />
-
-                            <input
-                                placeholder='Shipping Address'
-                                {...register("address", { required: true })}
-                                className='border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 w-3/5'
-                                type='text'
-                            /> <br />
-
-                            {errors.exampleRequired && <span>This field is required</span>}
-
-                            <input type="submit" className="py-2 px-4  bg-gray-800 hover:bg-gray-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-3/5 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg " />
-                        </form>
-
-                        {/* react hook form ends*/}
-
-                    </div>
-                </div>
+  return (
+    <>
+      <NavBar />
+      <div className="container mx-auto h-screen mt-10 mb-20">
+        <h2 className="flex items-center justify-center text-5xl font-bold">
+          Confirm your booking for Weeding Photography
+        </h2>
+        <div className="flex justify-between flex-wrap mt-20">
+          <div className="flex flex-col items-center justify-center max-w-[50%]  p-3">
+            <img
+              className="inline-block h-96 w-full rounded-md"
+              src={service?.img}
+              alt=""
+            />
+            <h2 className="text-center text-4xl mt-4 font-semibold">
+              {service?.title}
+            </h2>
+            <h2 className="text-center text-2xl mt-4">
+              {service?.description}
+            </h2>
+            <div className="text-2xl mt-2 text-center ">
+              Ratings:{" "}
+              <Rating
+                readonly
+                placeholderRating={service?.ratings}
+                emptySymbol={<i className="far fa-star text-orange-400"></i>}
+                placeholderSymbol={
+                  <i className="fas fa-star text-orange-400 " />
+                }
+                fullSymbol={<i className="fas fa-star bg-orange-400" />}
+              />
+              <h4 className="text-2xl my-5">Price: ${service?.price}</h4>
             </div>
-            <Footer />
-        </>
-    );
+          </div>
+
+          {/* react hook form starts*/}
+
+          <div className="flex flex-col items-center justify-center min-w-[50%]">
+            <h2 className="text-3xl font-bold mb-5">Billing Information</h2>
+
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="w-full flex flex-col items-center justify-center"
+            >
+              <input
+                defaultValue="akbarkha@gmail.com"
+                {...register("email", { required: true })}
+                className="border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 w-3/5"
+                type="email"
+                placeholder="Your Email"
+              />{" "}
+              <br />
+              <input
+                defaultValue="Akbar Kha"
+                {...register("name", { required: true })}
+                className="border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 w-3/5"
+                type="text"
+                placeholder="Your Name"
+              />{" "}
+              <br />
+              <input
+                placeholder="Your Phone"
+                {...register("phone", { required: true })}
+                className="border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 w-3/5"
+                type="number"
+              />{" "}
+              <br />
+              <input
+                placeholder="Shipping Address"
+                {...register("address", { required: true })}
+                className="border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 w-3/5"
+                type="text"
+              />{" "}
+              <br />
+              {errors.exampleRequired && <span>This field is required</span>}
+              <input
+                type="submit"
+                className="py-2 px-4  bg-gray-800 hover:bg-gray-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-3/5 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+              />
+            </form>
+
+            {/* react hook form ends*/}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Purchase;
